112e575dfba6a9dd207f7d9c2025b54ce71abc54
Sathwik <sathwikchirivelli@gmail.com>
2025-02-11 18:53:54 +0530

    installed prettier & added sqlite from bun

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,15 @@
+import { Database } from 'bun:sqlite'
 import { Hono } from 'hono'
+
+const db = new Database(':memory:')
+const query = db.query("SELECT 'Hello world' as message;")
+const result = query.get() // => { message: "Hello world" }
+console.log(result)
 
 const app = new Hono()
 
 app.get('/', ctx => {
-  return ctx.text('Hello Hono!')
+    return ctx.text('Hello Hono!')
 })
 
 export default app
