45e628f69271c5274340b419eb55313b41365a1c
Sathwik <sathwikchirivelli@gmail.com>
2025-02-14 21:55:04 +0530

    Added a new endpoint /:id

--- src/index.ts
+++ src/index.ts
@@ -4,35 +4,41 @@ const app = new Hono()
 
 app.use('/*', cors())
 
+const subscriptions = [
+    {
+        name: 'The Verge',
+        category: 'Tech + Culture',
+        feedLink: 'https://www.theverge.com/rss/index.xml',
+    },
+    {
+        name: 'CNET',
+        category: 'Smart Home',
+        feedLink: 'https://www.cnet.com/rss/news/',
+    },
+    {
+        name: 'Engadget',
+        category: 'Tech',
+        feedLink: 'https://www.engadget.com/rss/',
+    },
+    {
+        name: 'Tech Crunch',
+        category: 'Startup',
+        feedLink: 'https://techcrunch.com/feed/',
+    },
+]
+
 app.get('/subscriptions', ctx => {
-    return ctx.json([
-        {
-            name: 'The Verge',
-            category: 'Tech + Culture',
-            feedLink: 'https://www.theverge.com/rss/index.xml',
-        },
-        {
-            name: 'CNET',
-            category: 'Smart Home',
-            feedLink: 'https://www.cnet.com/rss/news/',
-        },
-        {
-            name: 'Engadget',
-            category: 'Tech',
-            feedLink: 'https://www.engadget.com/rss/',
-        },
-        {
-            name: 'Tech Crunch',
-            category: 'Startup',
-            feedLink: 'https://techcrunch.com/feed/',
-        },
-    ])
+    return ctx.json(subscriptions)
 })
 
-app.get('/theverge', async ctx => {
-    const tvRes = await fetch('https://www.theverge.com/rss/index.xml')
+app.get('/subscriptions/:idx', async ctx => {
+    const idx = ctx.req.param('idx')
+    console.log(idx)
+    // @ts-ignore
+    const tvRes = await fetch(subscriptions[idx].feedLink)
     const tvXml = await tvRes.text()
     return ctx.text(tvXml)
+    // return ctx.text('testing')
 })
 
 export default app
