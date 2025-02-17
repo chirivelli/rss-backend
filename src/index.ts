b742c714b52f643fa431c96d059cdedfa38d5d38
Sathwik <sathwikchirivelli@gmail.com>
2025-02-17 17:11:05 +0530

    storing sites in SQLite

--- src/index.ts
+++ src/index.ts
@@ -1,44 +1,47 @@
 import { Hono } from 'hono'
 import { cors } from 'hono/cors'
+import { Database } from 'bun:sqlite'
+
 const app = new Hono()
+const db = new Database('rss_db.sqlite')
+
+db.run(`
+    CREATE TABLE IF NOT EXISTS sites (
+    id INTEGER PRIMARY KEY,
+    name VARCHAR(100),
+    feedLink VARCHAR NOT NULL)
+`)
+
+// db.run(`
+//     INSERT INTO sites (name, feedLink)
+//     VALUES
+//         ('The Verge', 'https://www.theverge.com/rss/index.xml'),
+//         ('CNET', 'https://www.cnet.com/rss/news/'),
+//         ('Engadget', 'https://www.engadget.com/rss/'),
+//         ('Tech Crunch', 'https://techcrunch.com/feed/')
+// `)
 
 app.use('/*', cors())
 
-const subscriptions = [
-    {
-        name: 'The Verge',
-        category: 'Tech + Culture',
-        feedLink: 'https://www.theverge.com/rss/index.xml',
-    },
-    {
-        name: 'CNET',
-        category: 'Smart Home',
-        feedLink: 'https://www.cnet.com/rss/news/',
-    },
-    {
-        name: 'Engadget',
-        category: 'Tech',
-        feedLink: 'https://www.engadget.com/rss/',
-    },
-    {
-        name: 'Tech Crunch',
-        category: 'Startup',
-        feedLink: 'https://techcrunch.com/feed/',
-    },
-]
-
-app.get('/subscriptions', ctx => {
-    return ctx.json(subscriptions)
+app.get('/', ctx => {
+    return ctx.text('Hono is Running!')
 })
 
-app.get('/subscriptions/:idx', async ctx => {
-    const idx = ctx.req.param('idx')
+app.get('/subscriptions', ctx => {
+    const query = db.query(`SELECT * FROM sites`)
+    return ctx.json(query.all())
+})
+
+app.get('/subscriptions/:id', async ctx => {
+    const idx = ctx.req.param('id')
     console.log(idx)
+
+    const query = db.query(`SELECT * FROM sites WHERE id=${idx}`)
+    const site = query.get()
     // @ts-ignore
-    const tvRes = await fetch(subscriptions[idx].feedLink)
-    const tvXml = await tvRes.text()
-    return ctx.text(tvXml)
-    // return ctx.text('testing')
+    const response = await fetch(site.feedLink)
+    const xmlString = await response.text()
+    return ctx.text(xmlString)
 })
 
 export default app
