7e351353a685d196f95ff08eca3349b3f1cf63de
Sathwik <sathwikchirivelli@gmail.com>
2025-03-27 22:07:11 +0530

    Article endpoint now only works with 1 feed link

--- src/controllers/articles.js
+++ src/controllers/articles.js
@@ -1,38 +1,40 @@
 import { Hono } from 'hono'
-import { User } from '../mongodb'
 import { XMLParser } from 'fast-xml-parser'
 
 const articles = new Hono()
 
-async function getPosts(username) {
-    const user = await User.findOne({ username: username })
-    const subs = user.subscriptions
-    const parser = new XMLParser({ ignoreAttributes: true })
+async function getPosts(blogLink) {
     const posts = []
-    for (const sub of subs) {
-        const res = await fetch(sub.link)
-        const xmlText = await res.text()
-        const jsonObj = parser.parse(xmlText)
-        const feed = jsonObj.rss?.channel ?? jsonObj.feed
-        const postsList = feed.item ?? feed.entry
-        for (const post of postsList) {
-            // console.log(post)
-            posts.push({
-                title: post.title,
-                site: sub.name,
-                pubDate: post.pubDate ?? post['published'],
-                snippet: post.description ?? post.summary,
-                author: post['dc:creator'] ?? post.author?.name,
-                link: post.link || post['id'],
-            })
-        }
+    const parser = new XMLParser({ ignoreAttributes: true })
+    const res = await fetch(blogLink)
+    const xmlText = await res.text()
+    const jsonObj = parser.parse(xmlText)
+
+    let feedName = jsonObj.rss?.channel?.title ?? jsonObj.feed.title
+    feedName = feedName.split(' ').length < 4 ? feedName : feedName.split(' ')[0]
+
+    const feed = jsonObj.rss?.channel ?? jsonObj.feed
+    const postsList = feed.item ?? feed.entry
+    for (const post of postsList) {
+        // console.log(post)
+
+        const date = new Date(post.pubDate ?? post.published)
+
+        posts.push({
+            title: post.title,
+            site: feedName,
+            published: date,
+            snippet: post.description ?? post.summary,
+            author: post['dc:creator'] ?? post.author?.name,
+            link: post.link || post['id'],
+        })
     }
     return posts
 }
 
 articles.get('/', async ctx => {
-    const username = ctx.req.header('X-Username')
-    const posts = await getPosts(username)
+    const blogLink = ctx.req.header('X-FeedLink')
+    const posts = await getPosts(blogLink)
     return ctx.json(posts)
 })
 
