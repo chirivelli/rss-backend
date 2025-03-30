9a7a4b4ce27cf489fba0a25269acce58b76aa5e9
Sathwik <sathwikchirivelli@gmail.com>
2025-03-30 16:00:11 +0530

    using MongoClient api in subs GET endpoint

--- src/controllers/subscriptions.ts
+++ src/controllers/subscriptions.ts
@@ -1,12 +1,13 @@
 import { Hono } from 'hono'
 import { User } from '../mongodb'
+import { db } from '../db'
 
 const subscriptions = new Hono()
 
 subscriptions.get('/', async ctx => {
     const username = ctx.req.header('X-Username')
-
-    const user = await User.findOne({ username: username })
+    const usersCollection = db.collection('users')
+    const user = await usersCollection.findOne({ username: username })
 
     if (!user) return ctx.json([])
 
