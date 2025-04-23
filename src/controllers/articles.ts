437fa667a1c2e83c17cc8ecde7388b60fd4048a7
Sathwik <sathwikchirivelli@gmail.com>
2025-04-23 12:05:49 +0530

    minor refactor

--- src/controllers/articles.ts
+++ src/controllers/articles.ts
@@ -10,45 +10,27 @@ async function getPosts(blogLink: string) {
     const res = await fetch(blogLink)
     const xmlText = await res.text()
     const jsonObj = parser.parse(xmlText)
-
     let feedName = jsonObj.rss?.channel?.title ?? jsonObj.feed.title
     feedName =
         feedName.split(' ').length < 4 ? feedName : feedName.split(' ')[0]
-
     const feed = jsonObj.rss?.channel ?? jsonObj.feed
     const postsList = feed.item ?? feed.entry
+
     for (const post of postsList) {
-        // console.log(post)
-
-        const date = new Date(post.pubDate ?? post.published)
-
         posts.push({
             title: post.title,
             site: feedName,
-            published: date,
+            published: new Date(post.pubDate ?? post.published),
             snippet: post.description ?? post.summary,
             author: post['dc:creator'] ?? post.author?.name,
             link: post.link || post['id'],
             imageLink: jsonObj.feed?.icon ?? jsonObj.rss?.channel?.image?.url,
         })
-
-        // if (posts.length == 10) return posts
     }
     return posts
 }
 
 articles.get('/', async ctx => {
-    const blogLink = ctx.req.header('X-FeedLink')
-    if (blogLink === undefined) {
-        return ctx.json([])
-    }
-    const posts = await getPosts(blogLink)
-    return ctx.json(posts)
-})
-
-articles.get('/sorted', async ctx => {
-    // merge k sorted list
-    // naive sorting!
     const username = ctx.req.header('X-Username')
 
     const user = await db.collection('users').findOne({ username: username })
